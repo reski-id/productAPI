9784604cf6e004c80b47007450ce172010dc1d32
reski-id <pesan.reski@gmail.com>
2023-03-13 22:08:08 +0700

    init golang echo

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"productapi/config"
+	"productapi/infrastruktur/database/mysql"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	cfg := config.GetConfig()
+	db := mysql.InitDB(cfg)
+	mysql.MigrateData(db)
+	e := echo.New()
+
+	fmt.Println("Running program ....")
+	dsn := fmt.Sprintf(":%d", config.SERVERPORT)
+	e.Logger.Fatal(e.Start(dsn))
+}
